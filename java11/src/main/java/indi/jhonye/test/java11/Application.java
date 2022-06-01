67da6573261894c0ff6f7d8f4ef7fd641d4970cb
jhonye <jhoye@jhoyedeMacBook-Pro.local>
2022-06-01 11:21:28 +0800

    init

---
+++ java11/src/main/java/indi/jhonye/test/java11/Application.java
@@ -0,0 +1,9 @@
+package indi.jhonye.test.java11;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("java11!");
+    }
+
+}
---
+++ java8/src/main/java/indi/jhonye/test/java8/Application.java
@@ -0,0 +1,9 @@
+package indi.jhonye.test.java8;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("java8!");
+    }
+
+}
